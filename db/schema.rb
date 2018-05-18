388105dc1f6b6b9668d84a09d19e6a1eb031964e
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-18 10:18:13 -0400

    switched to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_05_18_050326) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "destinations", force: :cascade do |t|
     t.string "title"
     t.string "description"
