1204a44727b42e6449f63e2c827cbb03e0dea922
Olga Boiaryntseva <olgaboiar@gmail.com>
2018-05-22 12:08:02 -0400

    fixed config application rb

--- config/application.rb
+++ config/application.rb
@@ -6,8 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.paths << Rails.root.join("app", "assets", "img")
-
 module Dragonflies
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
